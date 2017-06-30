3e08a7bab95b71fbacb6568c47118d77e85f21ac
rlucas <r.lucas@code-troopers.com>
2017-06-30 11:13:15 +0200

    remove export

--- src/datepicker.module.ts
+++ src/datepicker.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 
 import { DatepickerComponent } from './datepicker.component';
 
-export * from './datepicker.component';
-
 @NgModule({
   declarations: [ DatepickerComponent ],
   exports: [ DatepickerComponent ],
